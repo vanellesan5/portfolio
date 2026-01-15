45643ebf093313b8dfbd27e7e579ed904b2bffde
Vnellesan5 <sandravanelle0@gamil.com>
2026-01-15 23:37:04 +0100

    modification

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>jesen Omega - Software developer</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+   <header>
+    <div class="logo">jesen Omega</div>
+    <nav>
+        <ul>
+            <li><a href="#home">home</a></li> 
+            <li><a href="#about">about</a></li> 
+            <li><a href="#projects">projects</a></li> 
+            <li><a href="#contacts">contacts</a></li>
+        </ul> 
+    </nav>
+  </header>  
+  
+  <section class="hero">
+    <div class="hero-content">
+        <h1>Hello<span clas="dot">.</span></h1>
+        <div class="line"></div>
+        <h2>I'm jesen</h2>
+        <h3>Software developer</h3>
+
+        <div class="hero-buttons">
+            <a href="#" class="btn btn-primary">Got a project?</a>
+            <a href="#" class="btn btn-secondary">My resume</a>
+        </div>    
+        <div class="tech-stack">
+            <span>HTML5</span>
+            <span>CSS</span>
+            <span>Javascript</span>
+            <span>Node.js</span>
+            <span>React</span>
+            <span>Git</span>
+            <span>Github</span>
+        
+        </div>
+    </div>
+    <div class="hero-image">
+        <div class="bracket bracket-left">&lt;</div>
+        <div class="circle-bg"></div>
+        <div class="bracket bracket-right">&gt;</div>
+    </div>
+</section>
+    
+<section class="services">
+    <div class="timeline">
+        <div class="service-item">
+            <div class="service-icon">&lt;/&gt;</div>
+            <div class="service-text">
+                <h3>Wensite development</h3>
+            </div>   
+        </div>  
+        <div class="service-item">
+            <div class="service-icon"></div>
+            <div class="service-text">
+                <h3>App development</h3>
+            </div>
+        </div>
+        <div class="service-item">
+            <div class="service-icon"></div>
+            <div class="service-text">
+                <h3>Website Hosting</h3>
+            </div>
+        </div>
+    </div>
+
+    <div class="about">
+        <h2>About me</h2>
+        <p>I started my software journey from photography. Through that, i learned to love the process ofof creating from scratch. since then, this had led me to software development as it fulfills my love for learning and building things.</p>
+        
+
+</body>
+</html>
